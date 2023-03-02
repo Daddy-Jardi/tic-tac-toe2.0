4f51c2ffe6f8598806a8682c310b9dd696a58773
Daddy-Jardi <jardielgaming@gmail.com>
2023-03-01 22:19:21 -0500

    clear screen after each input

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from helper import draw_board
+import os
 
 spots = {1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9'}
 
-
 playing = True
+
 while playing:
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     draw_board(spots)
     #player input
     choice = input()
