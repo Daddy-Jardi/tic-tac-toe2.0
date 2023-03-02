04ae7a668f35105c112fe2d1a13cc44ede01abef
Daddy-Jardi <jardielgaming@gmail.com>
2023-03-01 22:33:15 -0500

    error control on player input

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from helper import draw_board
+from helper import draw_board, check_turn
 import os
 
 spots = {1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9'}
 
 playing = True
+turn = 0
 
 while playing:
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -13,3 +14,11 @@ while playing:
     choice = input()
     if choice == 'q':
         playing = False
+    elif str.isdigit(choice) and int(choice) in spots:
+        if not spots[int(choice)] in {'X', 'O'}:
+            turn += 1
+            spots[int(choice)] = check_turn(turn)
+
+
+    
+
